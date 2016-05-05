780f5f786c2fcefd22e997bee50288c05fce3e5d
배지영 <melissaeliza0@gmail.com>
2016-05-06 02:10:34 +0900

    recursion 369.

--- 2week/Example/369Game/369Game/main.m
+++ 2week/Example/369Game/369Game/main.m
@@ -9,8 +9,10 @@
 #import <Foundation/Foundation.h>
 
 void game369(int num);
+bool is369(int num);
 
 int getFirstInteger(int num);
+bool isZzark(int num);
 
 
 int main(int argc, const char * argv[])
@@ -18,20 +20,22 @@ int main(int argc, const char * argv[])
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
-    game369(10);
+        
+        game369(339);
+        printf("-------------------------");
+        
+        
     }
     return 0;
 }
 
-//29 이하의 369 게임
 void game369(int num)
 {
     char zark = '*';
     
-    for (int i=1; i<30; i++)
+    for (int i=1; i<=num; i++)
     {
-        int firstNum = getFirstInteger(i);
-        if (firstNum % 3 == 0 && firstNum != 0)
+        if (isZzark(i))
         {
             printf("%c ",zark);
             continue;
@@ -40,7 +44,28 @@ void game369(int num)
     }
 }
 
+// 1의 자릿수 구하기
 int getFirstInteger(int num)
 {
     return num % 10;
 }
+
+bool isZzark(int num)
+{
+    if (is369(num))
+    {
+        return true;
+    }
+    else if (num >= 10)
+    {
+        return isZzark(num / 10);
+    }
+    return false;
+}
+
+bool is369(int num)
+{
+    int first = getFirstInteger(num);
+    return (first % 3 == 0 && first != 0);
+}
+
