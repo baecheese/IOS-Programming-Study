05e5837ce9aca9b3c67fa175a4f1eb3b2a2bd365
배지영 <melissaeliza0@gmail.com>
2016-05-09 13:28:05 +0900

    Revise Bug

--- 2week/Example/D-DayProject/D-DayProject/main.m
+++ 2week/Example/D-DayProject/D-DayProject/main.m
@@ -9,28 +9,16 @@
 #import <Foundation/Foundation.h>
 
 void dDayInput(int today, int eventday);//숫자형식 YYYYMMDD
-void monthGap(int todayMonth, int eventdayMonth);
 bool daysOfMonth (int startMonth, int endMonth);
 bool anyLike(int i, int array[]);
-void DayGap(int todayDay, eventdayDay);
 bool isError(int startMonth,int endMonth);
-void monthlyDay (int start, int end);
+int monthlyDay (int year, int start, int end);
 
 bool isLeapYear(int year);
-bool check2month(int todayMonth, int eventdayMonth);
-bool is2Month(int starMonth, int endMonth);
-void startMonthDay(int startMonth);
+int startMonthDay(int startMonth);
 
+// result
 int total = 0;//마지막 함수에서 출력
-
-int todayInput;
-int eventdayInput;
-int todayYear;
-int eventdayYear;
-int todayMonth;
-int eventdayMonth;
-int todayDay;
-int eventdayDay;
 
 int main(int argc, const char * argv[])
 {
@@ -39,9 +27,8 @@ int main(int argc, const char * argv[])
         // insert code here...
         NSLog(@"Hello, World!");
         
-        dDayInput(20160101, 20170321);
+        dDayInput(20160101, 20160319);
         printf(" --------- ");
-        printf("D-Day %d 일 입니다. \n", total);
     }
     return 0;
 }
@@ -50,105 +37,99 @@ int main(int argc, const char * argv[])
 //YYYYMMDD
 void dDayInput(int today, int eventday)
 {
-    todayInput = today;
-    eventdayInput = eventday;
-    
     //YYYY
-    todayYear = today / 10000;
-    eventdayYear = eventday/10000;
+    int todayYear = today / 10000;
+    int eventdayYear = eventday/10000;
     
     //MM
-    todayMonth = (today%10000)/100;
-    eventdayMonth = (eventday%10000)/100;
+    int todayMonth = (today%10000)/100;
+    int eventdayMonth = (eventday%10000)/100;
     
     // DD
-    todayDay = today%100;
-    eventdayDay = eventday%100;
+    int todayDay = today%100;
+    int eventdayDay = eventday%100;
+    
+    //첫 달 일자, 끝 달 일자 ++ ----------------
+    total += startMonthDay(todayMonth)-todayDay+1;//오늘포함
+    total += eventdayDay;
+    
+    
+    ///--- 년도 별 간격------------------------
+    
+    // 1년 이상일 때
+    if (1 <= eventdayYear-todayYear)
+    {
+        total += (eventdayYear - todayYear)*365;
+    }
+    
+    //년도가 같을 때(1년 이하, 개월 수에 따른)
+    if (0 == eventdayYear-todayYear && 12 > todayMonth + eventdayMonth)
+    {
+        //2번째~마지막 바로 전까지
+        int SecondMonth = todayMonth + 1;
+        int PreeventdayMonth = eventdayMonth - 1;
+        //secondMonth~preEventMonth
+        total += monthlyDay(todayYear, SecondMonth, PreeventdayMonth);
+        printf("D-Day는 %d 이다.",total);
+    }
+    
+    ///------------------------------------
+    
+    if (today > eventday)
+    {
+        //스왑 ------> 부호 처리 나중에 어떻게 해야 하낭..움
+        dDayInput(eventday, today);
+    }
     
     if (today == eventday) {
         total = 0;
     }
-    
-    else if ((12-todayMonth+1)+eventdayMonth >= 12)//12개월 이상인 경우
-    {
-        //YYYYMMDD
-        monthGap(today, eventday);
-    }
-    else if ((12-todayMonth+1)+eventdayMonth < 12)//12개월 이하인 경우
-    {
-        // --- 일수 체크해서 total 업데이트 -> 윤달 체크도 마무리 하나 체크하기 --------------------------todo
-    }
-    else if (today > eventday)
-    {
-        //print("과거 이벤트에 대한 디데이 입니다.");
-        dDayInput(eventday, today);
-    }
 }
 
-
-//YYYYMMDD
-void monthGap(int today, int eventday)
-{
-    //YYYYMM
-    int todayYearAndMonth = today / 100;
-    int eventdayYearAndMonth = eventday / 100;
-    
-    if (todayYearAndMonth < eventdayYearAndMonth)
-    {
-        
-        // 달별 일수 ++
-        monthlyDay(todayMonth+1, eventdayMonth);
-        
-        // 시작달 일수 ++
-        startMonthDay(todayMonth);
-        
-    
-    }
-    else if (todayYearAndMonth > eventdayYearAndMonth)
-    {
-        monthGap(eventdayYearAndMonth, todayYearAndMonth);
-    }
-}
-
-//시작 달 전체 - 시작달 날짜 --> 토탈++
-void startMonthDay(int startMonth)
+//시작 달 전체
+int startMonthDay(int startMonth)
 {
     int array[7] = {1, 3, 5, 7, 8, 10, 12};
     if (anyLike(startMonth, array))
     {
-        total += (31-todayDay+1);
+        return 31;
     }
     else
     {
-        total += (30-eventdayDay+1);
+        return 30;
     }
     
 }
 
 
-// 월별 날짜 계산
-void monthlyDay (int year, int month)
+// 월별 날짜 계산-------todo
+int monthlyDay (int year, int start, int end)
 {
-    int array[7] = {1, 3, 5, 7, 8, 10, 12};
-    if (anyLike(month, array))
+    for (int i=start; i<=end; i++)
     {
-        total =+ 31;
-    }
-    else if (month == 2)
-    {
-        if (isLeapYear(year))
+        int month = i;
+        int array[7] = {1, 3, 5, 7, 8, 10, 12};
+        if (anyLike(month, array))
         {
-            total =+  29;
+            return 31;
+        }
+        else if (month == 2)
+        {
+            if (isLeapYear(year))
+            {
+                return 29;
+            }
+            else
+            {
+                return 28;
+            }
         }
         else
         {
-            total =+  28;
+            return 30;
         }
     }
-    else
-    {
-        total =+  30;
-    }
+    return 0;
 }
 
 
@@ -172,5 +153,5 @@ bool isError(int startMonth,int endMonth)
 
 bool isLeapYear(int year)
 {
-    return (year%4==0 && year%100!=0 && year%400==0);
+    return (year%4==0 && year%100!=0)||(year%400==0);
 }
