4634919889ef62274105d6132ef0998613630d97
배지영 <melissaeliza0@gmail.com>
2016-06-17 13:14:27 +0900

    thread 2개 만들어서 사용하는거 _미완성 (thread  하나에 메소드 2개 사용하는 것으로 변경 예정)

--- 8week/NSThread/NSThread/StopWatchViewController.m
+++ 8week/NSThread/NSThread/StopWatchViewController.m
@@ -13,6 +13,10 @@
 @property (nonatomic, strong) NSThread *thread;
 @property (weak, nonatomic) IBOutlet UILabel *countNumLabel;
 @property (nonatomic) NSInteger number;
+@property (nonatomic) NSInteger number2;
+
+@property (nonatomic, strong) NSThread *thread2;
+@property (weak, nonatomic) IBOutlet UILabel *mutiThreadLabel;
 
 @end
 
@@ -38,7 +42,7 @@
     else {
     /* 스레드 객체로 만들어서 하는 방법 */
      self.thread = [[NSThread alloc]
-     initWithTarget:self selector:@selector(watchAction:) object:nil];
+     initWithTarget:self selector:@selector(watchAction:) object:self.thread];
      [self.thread start];
     }
 }
@@ -62,22 +66,60 @@
     NSLog(@"-------reset--------");
 }
 
+
+// waitUntilDone NO
+- (IBAction)waitNOBtn:(id)sender
+{
+    // 시작 또 누르면
+    if (self.thread2 != nil) {
+        UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"경고" message:@"현재 타이머 진행 중입니다. 멈추시고 다시 시작해주세요." delegate:self cancelButtonTitle:nil otherButtonTitles:@"YES", nil];
+        
+        // alert창을 띄우는 method는 show이다.
+        [alert show];
+    }
+    else {
+        /* 스레드 객체로 만들어서 하는 방법 */
+        
+        
+        // 스레드 객체 생성 전에 만들어서 nil이 들어옴
+        // 스레드로 구분 하는 것은 아니니 메소드 두개로 구분하는게 옳아 보인다.
+        self.thread2 = [[NSThread alloc]
+                       initWithTarget:self selector:@selector(watchAction:) object:self.thread2];
+        [self.thread2 start];
+    }
+}
+
 // 위에 number로 시작하게
 -(void)watchAction:(NSThread *)thread
 {
-    NSInteger i = self.number;
-    while (self.thread.isCancelled == NO) {
-        i++;
+    while (self.thread.isCancelled || self.thread2.isCancelled == NO) {
+        
+        if (thread == self.thread) {
+            self.number++;
+        }
+        if (thread == self.thread2) {
+            self.number2++;
+        }
+        
         /* waitUntilDone이 YES면 main thread가 돌 때에는 main에 작업을 추가할 수 없다. 독점하는 것. */
-        [self performSelectorOnMainThread:@selector(changeDisplay:) withObject:[NSNumber numberWithInteger:i] waitUntilDone:YES];
+        
+        [self performSelectorOnMainThread:@selector(changeDisplay:) withObject:thread waitUntilDone:NO];
         [NSThread sleepForTimeInterval:1];
     }
     self.thread = nil;
+    self.thread2 = nil;
 }
 
--(void) changeDisplay:(NSNumber *)number
+-(void)changeDisplay:(NSThread *)thread
 {
-    [self.countNumLabel setText:[NSString stringWithFormat:@"%@", number]];
+    if (thread == self.thread) {
+        [self.countNumLabel setText:[NSString stringWithFormat:@"%ld", (long)self.number]];
+    }
+    else
+    {
+        [self.mutiThreadLabel setText:[NSString stringWithFormat:@"%ld", (long)self.number2]];
+    }
+    
 }
 
 - (void)didReceiveMemoryWarning {
